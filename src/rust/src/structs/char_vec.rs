b7eb7f14552f632cee7bf7dd7bd927086e23bb8c
Eric Burden <eric.w.burden@gmail.com>
2021-03-31 12:34:12 -0500

    Stop rustfmt from line-breaking this function

--- src/rust/src/structs/char_vec.rs
+++ src/rust/src/structs/char_vec.rs
@@ -1,22 +1,22 @@
 //! This module demonstrates implementing a new struct that can be passed in and
-//! out of Rust functions annotated with #[extendr]. As of the writing of this 
-//! module, extendr_api (v0.2.0) does not support passing in/out a character 
-//! vector that may contain NA's by default. To allow for this, I've implemented 
-//! [FromRobj](extendr__api::robj::FromRobj) for a newtype 
+//! out of Rust functions annotated with #[extendr]. As of the writing of this
+//! module, extendr_api (v0.2.0) does not support passing in/out a character
+//! vector that may contain NA's by default. To allow for this, I've implemented
+//! [FromRobj](extendr__api::robj::FromRobj) for a newtype
 //! [CharVec](crate::char_ved::CharVec), which wrap a Vec<Option<String>> where
-//! NA's are represented by `None`. I've also implemented `From<CharVec>` for 
+//! NA's are represented by `None`. I've also implemented `From<CharVec>` for
 //! `Robj`, allowing extendr to cast a `CharVec` to an `Robj` representing a
 //! character vector.
 
 use extendr_api::prelude::{na_str, FromRobj, Robj};
 use std::iter::FromIterator;
 
-/// Rust type that wraps a Vec<Option<String>>, where `None` represents an 
+/// Rust type that wraps a Vec<Option<String>>, where `None` represents an
 /// NA_character_ from R
 pub struct CharVec(Vec<Option<String>>);
 
 /// For converting `CharVec` to `Robj`
-impl From<CharVec> for Robj{
+impl From<CharVec> for Robj {
     fn from(cv: CharVec) -> Robj {
         Robj::from(cv.0)
     }
@@ -39,6 +39,7 @@ impl<'a> FromRobj<'a> for CharVec {
 }
 
 // Helper function
+#[rustfmt::ignore]
 pub(crate) fn parse_rchar(rchar: String) -> Option<String> {
     if rchar == na_str() { None } else { Some(rchar) }
 }
@@ -47,7 +48,7 @@ pub(crate) fn parse_rchar(rchar: String) -> Option<String> {
 impl IntoIterator for CharVec {
     type Item = Option<String>;
     type IntoIter = std::vec::IntoIter<Option<String>>;
-    
+
     fn into_iter(self) -> Self::IntoIter {
         self.0.into_iter()
     }
@@ -55,7 +56,7 @@ impl IntoIterator for CharVec {
 
 /// Allows for collecting a CharVec from any iterator that yields <Option<String>>
 impl FromIterator<Option<String>> for CharVec {
-    fn from_iter<I: IntoIterator<Item=Option<String>>>(iter: I) -> Self {
+    fn from_iter<I: IntoIterator<Item = Option<String>>>(iter: I) -> Self {
         CharVec(iter.into_iter().collect())
     }
 }
